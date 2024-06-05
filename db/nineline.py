0b5272bcdc1e2e8b1610512cc11ee1fe388fe37b
Olivia Kroop <kroop_olivia@bah.com>
2024-06-05 16:28:39 +0200

    fetch one by id

--- db/nineline.py
+++ db/nineline.py
@@ -4,35 +4,34 @@ import sqlite3
 
 connection = sqlite3.connect("ninelines.db")
 TABLE_NAME = "ninelines"
+SCHEMA = {
+    "id": "integer primary key",
+       "audio_transcription": "text",
+        "audio_file": "text",
+        "mgrs": "text",
+        "lat" :"real",
+        "lon": "real",
+        "frequency": "real",
+        "urgent": "integer",
+        "urgent_surgical": "integer",
+        "priority": "integer",
+        "routine": "integer",
+        "convenience": "integer",
+        "litter": "integer",
+        "ambulatory": "integer",
+        "us_military": "integer",
+        "us_civilian": "integer",
+        "non_us_civilian": "integer",
+        "enemy_prisoner_of_war": "integer",
+        "equipment": "text",
+        "site_security": "text",
+        "pickup_mark": "text",
+        "cbrn": "text"
+}
 
 def create_table():
     cursor = connection.cursor()
-    query = " ".join(
-        f"""CREATE TABLE IF NOT EXISTS {TABLE_NAME} (
-        id integer primary key,
-        audio_transcription text,
-        audio_file text,
-        mgrs text,
-        lat real,
-        lon real,
-        frequency real,
-        urgent integer,
-        urgent_surgical integer,
-        priority integer,
-        routine integer,
-        convenience integer,
-        litter integer,
-        ambulatory integer,
-        us_military integer,
-        us_civilian integer,
-        non_us_civilian integer,
-        enemy_prisoner_of_war integer,
-        equipment text,
-        site_security text,
-        pickup_mark text,
-        cbrn text
-        )""".split()
-    )
+    query = f"CREATE TABLE IF NOT EXISTS {TABLE_NAME} ({', '.join(k + ' ' + v for k, v in SCHEMA.items())})"
     cursor.execute(query)
 
 def insert(
@@ -66,3 +65,10 @@ def insert(
     cursor.execute(query)
     connection.commit()
 
+def fetch_one_by_id(id_):
+    cursor = connection.cursor()
+    res = cursor.execute(f"SELECT * FROM {TABLE_NAME} where id='{id_}'")
+    res = res.fetchone()
+    fields = list(SCHEMA.keys())
+    values = res if res else [[None] * len(fields)]
+    return dict(zip(fields, values))
