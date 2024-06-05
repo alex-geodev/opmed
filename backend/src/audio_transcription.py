c52f18b36758c617cd004bce1662f01c70d83b5b
alex.geodev <alex.geodev@gmail.com>
2024-06-05 11:33:41 +0200

    nine line parsing

--- backend/src/audio_transcription.py
+++ backend/src/audio_transcription.py
@@ -1,6 +1,8 @@
 import wave
 import vosk
 import json
+from pydub import AudioSegment
+import os
 
 MODEL_PATH = "../../models/vosk-model-small-en-us-0.15"
 
@@ -29,4 +31,13 @@ def transcribe_from_wav(wav_path):
     transcribed.append(json.loads(recognizer.FinalResult())['text'])
 
     return " ".join(transcribed)
+
+
+def convert_to_wav(m4a_path):
+
+    m4a_file = m4a_path # I have downloaded sample audio from this link https://getsamplefiles.com/sample-audio-files/m4a
+    wav_filename = './data/output.wav'
+
+    sound = AudioSegment.from_file(m4a_file, format='m4a')
+    file_handle = sound.export(wav_filename, format='wav')
 
