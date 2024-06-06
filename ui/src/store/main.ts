7fc26c96b1c8f3da0fa8d19b6d34e6246d8d2217
Olivia Kroop <kroop_olivia@bah.com>
2024-06-06 15:15:04 +0200

    whoops

--- ui/src/store/main.ts
+++ ui/src/store/main.ts
@@ -1,42 +1,24 @@
-import { useApi } from './api'; import { reactive, toRefs } from 'vue'; const {
-ninelineApi } = useApi(); const state = reactive({ prompt: '', allNineLines: []
-as any, activeNineLine: undefined, loading: false, coas: [], chatResponse: ''
-}); export async function getNineLines() { state.loading = true; const resp =
-await ninelineApi.value.getNinelineGet() const { data } = resp;
-console.log(data); state.allNineLines = data; state.loading = false; } export
-async function getChatBotAnswer() { state.loading = true; state.chatResponse =
-'response placeholder'; } export async function getNineLine(id: string) {
-state.loading = true; console.log(id); const resp = await
-ninelineApi.value.getNinelineGet(id) const { data } = resp; console.log(data);
-state.allNineLines = data; state.loading = false; } export async function
-getCOAS(id: string) { state.loading = true; const resp = await
-ninelineApi.value.createNinelineCreatePost const { data } = resp;
-console.log(data); state.coas = data; state.loading = false; } export function
-useState() { return { ...toRefs(state), getNineLine, getNineLines, getCOAS,
-getChatBotAnswer, }; }
 import { useApi } from './api';
 import { reactive, toRefs } from 'vue';
 
-const {
-    ninelineApi
-  } = useApi();
+const { ninelineApi } = useApi();
 
 const state = reactive({
-    prompt: '',
-    allNineLines: [] as any,
-    activeNineLine: undefined,
-    loading: false,
-    coas: [],
-    chatResponse: ''
+  prompt: '',
+  allNineLines: [] as any,
+  activeNineLine: undefined,
+  loading: false,
+  coas: [],
+  chatResponse: '',
 });
 
 export async function getNineLines() {
-    state.loading = true;
-    const resp = await ninelineApi.value.getNinelineGet()
-    const { data } = resp;
-    console.log(data);
-    state.allNineLines = data;
-    state.loading = false;
+  state.loading = true;
+  const resp = await ninelineApi.value.getNinelineGet();
+  const { data } = resp;
+  console.log(data);
+  state.allNineLines = data;
+  state.loading = false;
 }
 
 export async function getChatBotAnswer() {
@@ -45,30 +27,30 @@ export async function getChatBotAnswer() {
 }
 
 export async function getNineLine(id: string) {
-    state.loading = true;
-    console.log(id);
-    const resp = await ninelineApi.value.getNinelineGet(id)
-    const { data } = resp;
-    console.log(data);
-    state.allNineLines = data;
-    state.loading = false;
-  }
+  state.loading = true;
+  console.log(id);
+  const resp = await ninelineApi.value.getNinelineGet(id);
+  const { data } = resp;
+  console.log(data);
+  state.allNineLines = data;
+  state.loading = false;
+}
 
 export async function getCOAS(id: string) {
-    state.loading = true;
-    const resp = await ninelineApi.value.createNinelineCreatePost
-    const { data } = resp;
-    console.log(data);
-    state.coas = data;
-    state.loading = false;
-  }
+  state.loading = true;
+  const resp = await ninelineApi.value.createNinelineCreatePost;
+  const { data } = resp;
+  console.log(data);
+  state.coas = data;
+  state.loading = false;
+}
 
 export function useState() {
-    return {
-      ...toRefs(state),
-      getNineLine,
-      getNineLines,
-      getCOAS,
-      getChatBotAnswer,
-    };
-  }
+  return {
+    ...toRefs(state),
+    getNineLine,
+    getNineLines,
+    getCOAS,
+    getChatBotAnswer,
+  };
+}
